LMODroid/platform_packages_apps_Launcher3

Launcher3: Update floating window drawable
Change-Id: Ie360f3cc0d04919fd216d3061adb240603e46684

quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -299,7 +299,7 @@ public interface TaskShortcutFactory {
 
         public FloatingSystemShortcut(RecentsViewContainer container, TaskContainer taskContainer) {
             // TODO new icon?
-            super(R.drawable.float_portrait_2_24px, R.string.floating_window,
+            super(R.drawable.picture_in_picture_mobile_24px, R.string.floating_window,
                     container, taskContainer.getItemInfo(), taskContainer.getTaskView());
             mTaskView = taskContainer.getTaskView();
         }

neoteric-marble-a15/android_packages_apps_Launcher3

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -324,7 +324,7 @@ public interface TaskShortcutFactory {
 
         public FloatingSystemShortcut(RecentsViewContainer container, TaskContainer taskContainer) {
             // TODO new icon?
-            super(R.drawable.ic_caption_desktop_button_foreground, R.string.recent_task_option_freeform,
+            super(R.drawable.picture_in_picture_mobile_24px, R.string.recent_task_option_freeform,
                     container, taskContainer.getItemInfo(), taskContainer.getTaskView());
             mTaskView = taskContainer.getTaskView();
         }